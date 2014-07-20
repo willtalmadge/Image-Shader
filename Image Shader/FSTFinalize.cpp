e195903a09e7ff5149ecab2c4eba4f564cbc5cfe
willtalmadge <willtalmadge@gmail.com>
2014-07-20 00:31:20 -0600

    Can transform in either direction

--- Image Shader/FSTFinalize.cpp
+++ Image Shader/FSTFinalize.cpp
@@ -16,8 +16,14 @@ std::function<void (ISComplex&, ISSingleton&)> fstFinalizeForward_Re16RgbaVFP4(G
 {
     //Width is the size of the target buffer, the source buffer must be half this
     //TODO: get an assertion for this assumption here
+    
     return [=] (ISComplex& input, ISSingleton& output) {
-        size_t N = width;
+        size_t N;
+        if (orientation == FSTFinalizeOrientation::Cols) {
+            N = width;
+        } else {
+            N = height;
+        }
 
         ISPBufferRef real = dynamic_cast<ISPBufferRef>(input.getReal());
         ISPBufferRef imag = dynamic_cast<ISPBufferRef>(input.getImag());
@@ -38,20 +44,40 @@ std::function<void (ISComplex&, ISSingleton&)> fstFinalizeForward_Re16RgbaVFP4(G
         float16x4_t* fim16 = (float16x4_t*)imag->baseAddress();
         float16x4_t* F16 = (float16x4_t*)out->baseAddress();
 
-        for (size_t i = 0; i < height; i++) {
-
-            float32x4_t fre = vcvt_f32_f16( fre16[iidx(i, 0)] );
-
-            float32x4_t accum = {0.0f, 0.0f, 0.0f, 0.0f};
-            
-            fre16[iidx(i, 0)] = vcvt_f16_f32( vmulq_n_f32(fre, 0.5f) );
-            fim16[iidx(i, 0)] = vcvt_f16_f32(accum);
-            
-            for (size_t j = 0; j < N/2 - 1; j++) {
-                fre = vcvt_f32_f16( fre16[iidx(i, j)] );
-                accum = vaddq_f32(accum, fre);
-                F16[oidx(i, 2*j)] = fim16[iidx(i, j)];
-                F16[oidx(i, 2*j + 1)] = vcvt_f16_f32( accum );
+        if (orientation == FSTFinalizeOrientation::Cols) {
+            for (size_t i = 0; i < height; i++) {
+                
+                float32x4_t fre = vcvt_f32_f16( fre16[iidx(i, 0)] );
+                
+                float32x4_t accum = {0.0f, 0.0f, 0.0f, 0.0f};
+                
+                fre16[iidx(i, 0)] = vcvt_f16_f32( vmulq_n_f32(fre, 0.5f) );
+                fim16[iidx(i, 0)] = vcvt_f16_f32(accum);
+                
+                for (size_t j = 0; j < N/2 - 1; j++) {
+                    fre = vcvt_f32_f16( fre16[iidx(i, j)] );
+                    accum = vaddq_f32(accum, fre);
+                    F16[oidx(i, 2*j)] = fim16[iidx(i, j)];
+                    F16[oidx(i, 2*j + 1)] = vcvt_f16_f32( accum );
+                }
+            }
+        } else {
+            //FIXME: you are peaking into the buffer here, the logical coordinate system is no longer valid, the y-axis runs vertical, the transform might be running backwards when the direction is columns.
+            for (size_t i = 0; i < width; i++) {
+                
+                float32x4_t fre = vcvt_f32_f16( fre16[iidx(0, i)] );
+                
+                float32x4_t accum = {0.0f, 0.0f, 0.0f, 0.0f};
+                
+                fre16[iidx(0, i)] = vcvt_f16_f32( vmulq_n_f32(fre, 0.5f) );
+                fim16[iidx(0, i)] = vcvt_f16_f32(accum);
+                
+                for (size_t j = 0; j < N/2 - 1; j++) {
+                    fre = vcvt_f32_f16( fre16[iidx(j, i)] );
+                    accum = vaddq_f32(accum, fre);
+                    F16[oidx(2*j, i)] = fim16[iidx(j, i)];
+                    F16[oidx(2*j + 1, i)] = vcvt_f16_f32( accum );
+                }
             }
         }
     };
@@ -93,7 +119,7 @@ std::function<void (ISComplex&, ISSingleton&)> fstFinalize_Re16RgbaVFP4(GLuint w
         //TODO: what about the nyquist component?
         //TODO: check the loop ranges for one off on size by miscounting nyquist
         //TODO: what about alignment and row padding? I think I need to add some extra for that CVPixelBufferGetBytesPerRow can be used to correct for line padding.
-
+        //Normalization is not needed because the underlying FFT is normalized.
         if (orientation == FSTFinalizeOrientation::Cols) {
             for (size_t i = 0; i < height; i++) {
                 
