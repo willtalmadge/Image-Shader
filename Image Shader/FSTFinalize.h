a7c02a24efbbaa3146e24f4ece3bb410d5cd327c
willtalmadge <willtalmadge@gmail.com>
2014-08-02 01:55:43 -0600

    ROI support

--- Image Shader/FSTFinalize.h
+++ Image Shader/FSTFinalize.h
@@ -1,10 +1,19 @@
-//
-//  FSTFinalize.h
-//  Image Shader
-//
-//  Created by William Talmadge on 7/13/14.
-//  Copyright (c) 2014 William Talmadge. All rights reserved.
-//
+/*
+ Copyright (C) 2014  William B. Talmadge
+ 
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
 
 #ifndef __Image_Shader__FSTFinalize__
 #define __Image_Shader__FSTFinalize__
@@ -12,9 +21,9 @@
 #include <functional>
 #include "ISSingleton.h"
 #include "ISComplex.h"
+#include "ISRect.h"
 
 enum class FSTFinalizeOrientation {Rows, Cols};
-std::function<void (ISComplex&, ISSingleton&)> fstFinalizeForward_Re16RgbaVFP4(GLuint width, GLuint height, FSTFinalizeOrientation orientation);
-std::function<void (ISSingleton&, ISComplex&)> fstFinalizeInverse_Re16RgbaNEON(GLuint width, GLuint height, FSTFinalizeOrientation orientation);
+std::function<void (ISComplex&, ISSingleton&, ISRect, ISRect)> fstFinalize_Re16RgbaVFP4(FSTFinalizeOrientation orientation, GLfloat scale = 1.0f);
 
 #endif /* defined(__Image_Shader__FSTFinalize__) */
