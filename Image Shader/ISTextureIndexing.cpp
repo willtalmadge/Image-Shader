888068713f5eba097ba1efdf781d6ed6b2795772
willtalmadge <willtalmadge@gmail.com>
2014-08-02 02:06:51 -0600

    Added 2D indexer. These are actually prefered as 1D indexers don't handle ROI easily.

--- Image Shader/ISTextureIndexing.cpp
+++ Image Shader/ISTextureIndexing.cpp
@@ -1,10 +1,19 @@
-//
-//  ISTextureIndexing.cpp
-//  Image Shader
-//
-//  Created by William Talmadge on 7/19/14.
-//  Copyright (c) 2014 William Talmadge. All rights reserved.
-//
+/*
+ Copyright (C) 2014  William B. Talmadge
+ 
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
 
 #include "ISTextureIndexing.h"
 #include "ISVertexArray.h"
@@ -41,6 +50,7 @@ void appendV2RowPos(vector<GLfloat>& vec, array<GLfloat, 2> range, GLuint width)
 void appendV3RowPos(vector<GLfloat>& vec, array<GLfloat, 2> range) {
     vec.insert(vec.end(), {0.0f, range[1], 0.5f});
 }
+
 //Indexes in the direction of rows, oriented in the direction of columns
 template <int V>
 void appendColTex(vector<GLfloat>& vec, array<GLfloat, 2> range, bool transpose);
@@ -110,21 +120,64 @@ template<> void appendRowTex<3>(vector<GLfloat>& vec, array<GLfloat, 2> range, b
     }
     
 }
-
+//Two dimensional indexer geometry positions
+void appendV0QuadPos(vector<GLfloat>& vec, array<GLfloat, 2> rangeX, array<GLfloat, 2> rangeY) {
+    vec.insert(vec.end(), {rangeX[0], rangeY[0], 0.5f});
+}
+void appendV1QuadPos(vector<GLfloat>& vec, array<GLfloat, 2> rangeX, array<GLfloat, 2> rangeY) {
+    vec.insert(vec.end(), {rangeX[1], rangeY[0], 0.5f});
+}
+void appendV2QuadPos(vector<GLfloat>& vec, array<GLfloat, 2> rangeX, array<GLfloat, 2> rangeY) {
+    vec.insert(vec.end(), {rangeX[1], rangeY[1], 0.5f});
+}
+void appendV3QuadPos(vector<GLfloat>& vec, array<GLfloat, 2> rangeX, array<GLfloat, 2> rangeY) {
+    vec.insert(vec.end(), {rangeX[0], rangeY[1], 0.5f});
+}
+//Two dimensional geometry texture coords
+template <int V>
+void appendQuadTex(vector<GLfloat>& vec, array<GLfloat, 2> range, array<GLfloat, 2> rangeY, bool transpose);
+template<> void appendQuadTex<0>(vector<GLfloat>& vec, array<GLfloat, 2> rangeX, array<GLfloat, 2> rangeY, bool transpose) {
+    if (!transpose) {
+        vec.insert(vec.end(), {rangeX[0], rangeY[0]});
+    } else {
+        vec.insert(vec.end(), {rangeY[0], rangeX[0]});
+    }
+}
+template<> void appendQuadTex<1>(vector<GLfloat>& vec, array<GLfloat, 2> rangeX, array<GLfloat, 2> rangeY, bool transpose) {
+    if (!transpose) {
+        vec.insert(vec.end(), {rangeX[1], rangeY[0]});
+    } else {
+        vec.insert(vec.end(), {rangeY[0], rangeX[1]});
+    }
+}
+template<> void appendQuadTex<2>(vector<GLfloat>& vec, array<GLfloat, 2> rangeX, array<GLfloat, 2> rangeY, bool transpose) {
+    if (!transpose) {
+        vec.insert(vec.end(), {rangeX[1], rangeY[1]});
+    } else {
+        vec.insert(vec.end(), {rangeY[1], rangeX[1]});
+    }
+}
+template<> void appendQuadTex<3>(vector<GLfloat>& vec, array<GLfloat, 2> rangeX, array<GLfloat, 2> rangeY, bool transpose) {
+    if (!transpose) {
+        vec.insert(vec.end(), {rangeX[0], rangeY[1]});
+    } else {
+        vec.insert(vec.end(), {rangeY[1], rangeX[0]});
+    }
+}
 
 ISTextureIndexer& ISTextureIndexer::from(int rangeStart) {
     _rangeStart = rangeStart;
     return *this;
 }
-ISTextureIndexer& ISTextureIndexer::to(int rangeEnd) {
+ISTextureIndexer& ISTextureIndexer::upTo(int rangeEnd) {
     //This is not inclusive, if the for loop has a condition i < N, then rangedEnd should be N, if it has i <= N, then rangeEnd should be N+1
     _rangeEnd = rangeEnd;
     if (_rangeEnd < _rangeStart && _stride > 0) {
-        _stride *= -1;
+        _stride *= -1.0f;
     }
     return *this;
 }
-ISTextureIndexer& ISTextureIndexer::by(int stride) {
+ISTextureIndexer& ISTextureIndexer::by(float stride) {
     _stride = stride;
     return *this;
 }
@@ -137,21 +190,35 @@ ISTextureIndexer& ISTextureIndexer::alongRows(GLuint texelCount) {
     _texelCount = texelCount;
     return *this;
 }
+ISTextureIndexer& ISTextureIndexer::alongRows() {
+    _direction = ISDirection::Rows;
+    _texelCount = _rangeEnd - _rangeStart;
+    return *this;
+}
 ISTextureIndexer& ISTextureIndexer::alongCols(GLuint texelCount) {
     _direction = ISDirection::Cols;
     _texelCount = texelCount;
     return *this;
 }
+ISTextureIndexer& ISTextureIndexer::alongCols() {
+    _direction = ISDirection::Cols;
+    _texelCount = _rangeEnd - _rangeStart;
+    return *this;
+}
 ISTextureIndexer& ISTextureIndexer::reads() {
-    _reads = true;
+    _usage = Usage::Read;
     return *this;
 }
 ISTextureIndexer& ISTextureIndexer::writes() {
-    _reads = false;
+    _usage = Usage::Write;
+    return *this;
+}
+ISTextureIndexer& ISTextureIndexer::math() {
+    _usage = Usage::Math;
     return *this;
 }
 bool ISTextureIndexer::isPosition() {
-    return !_reads;
+    return _usage == Usage::Write;
 }
 bool ISTextureIndexer::isColOriented() {
     return (_direction == ISDirection::Rows);
@@ -174,20 +241,49 @@ array<GLfloat, 2> ISTextureIndexer::texelRange(size_t scale) {
         (_stride*(_rangeEnd - 0.5f) + _offset + 0.5f)/scale};
     return result;
 };
+array<GLfloat, 2> ISTextureIndexer::mathRange() {
+    array<GLfloat, 2> result{
+        (_stride*(_rangeStart - 0.5f) + _offset),
+        (_stride*(_rangeEnd - 0.5f) + _offset)};
+    return result;
+};
 
 template<int V>
 void appendTex(vector<GLfloat>& vec, vector<ISTextureIndexer>& readIndexers, const vector<array<GLfloat, 2> >& texRange, ISDirection writeDirection) {
     for (size_t i = 0; i < readIndexers.size(); i++) {
         if (writeDirection == ISDirection::Rows) {
             appendColTex<V>(vec, texRange[i], readIndexers[i].direction() != writeDirection);
-        } else {
+        } else if (writeDirection == ISDirection::Cols) {
             appendRowTex<V>(vec, texRange[i], readIndexers[i].direction() != writeDirection);
         }
     }
 }
+template<int V>
+void appendTex(vector<GLfloat>& vec,
+               vector<ISTextureIndexer>& readIndexersX,
+               vector<ISTextureIndexer>& readIndexersY,
+               const vector<array<GLfloat, 2> >& texRangeU,
+               const vector<array<GLfloat, 2> >& texRangeV,
+               ISDirection xWriteDirection) {
+    assert(readIndexersX.size() == readIndexersY.size());
+    for (size_t i = 0; i < readIndexersX.size(); i++) {
+        appendQuadTex<V>(vec, texRangeU[i], texRangeV[i], readIndexersX[i].direction() != xWriteDirection);
+    }
+}
+template<int V>
+void appendMath(vector<GLfloat>& vec,
+               vector<ISTextureIndexer>& mathIndexersX,
+               vector<ISTextureIndexer>& mathIndexersY,
+               const vector<array<GLfloat, 2> >& mathRangeX,
+               const vector<array<GLfloat, 2> >& mathRangeY,
+               ISDirection xWriteDirection) {
+    assert(mathIndexersX.size() == mathIndexersY.size());
+    for (size_t i = 0; i < mathIndexersX.size(); i++) {
+        appendQuadTex<V>(vec, mathRangeX[i], mathRangeY[i], mathIndexersX[i].direction() != xWriteDirection);
+    }
+}
 
 void indexerGeometry(ISVertexArray* geometry, GLuint width, GLuint height, ISTextureIndexer writeIndexer, vector<ISTextureIndexer> readIndexers) {
-    //If tex orientation is opposite orientation of pos orientation the only action to take is to transpose u and v from what it would be if the orientation were matched to pos.
     vector<GLfloat> vec;
     assert(writeIndexer.isPosition());
     array<GLfloat, 2> posRange = writeIndexer.positionRange();
@@ -196,6 +292,7 @@ void indexerGeometry(ISVertexArray* geometry, GLuint width, GLuint height, ISTex
 
     vector<array<GLfloat, 2> > texRange(readIndexers.size());
     for (size_t i = 0; i < readIndexers.size(); i++) {
+        assert(readIndexers[i]._usage == ISTextureIndexer::Usage::Read);
         texRange[i] = readIndexers[i].texelRange(readIndexers[i].texelCount());
         geometry->addFloatAttribute(i + 1, 2);
     }
@@ -229,6 +326,66 @@ void indexerGeometry(ISVertexArray* geometry, GLuint width, GLuint height, ISTex
         appendV3RowPos(vec, posRange);
         appendTex<3>(vec, readIndexers, texRange, dir);
     }
+    
+    geometry->upload(vec);
+}
+void indexerGeometry2D(ISVertexArray* geometry, ISTextureIndexer writeIndexerX, ISTextureIndexer writeIndexerY, vector<ISTextureIndexer> readIndexersX, vector<ISTextureIndexer> readIndexersY) {
+    indexerGeometry2D(geometry, writeIndexerX, writeIndexerY, readIndexersX, readIndexersY, {}, {});
+}
+void indexerGeometry2D(ISVertexArray* geometry, ISTextureIndexer writeIndexerX, ISTextureIndexer writeIndexerY, vector<ISTextureIndexer> readIndexersX, vector<ISTextureIndexer> readIndexersY, vector<ISTextureIndexer> mathIndexersX, vector<ISTextureIndexer> mathIndexersY) {
+    vector<GLfloat> vec;
+    static const ISDirection xWriteDirection = ISDirection::Rows;
+    static const ISDirection yWriteDirection = ISDirection::Cols;
+    assert(writeIndexerX.isPosition());
+    assert(writeIndexerY.isPosition());
+    assert(writeIndexerX.direction() == xWriteDirection);
+    assert(writeIndexerY.direction() == yWriteDirection);
+    assert(readIndexersX.size() == readIndexersY.size());
+    array<GLfloat, 2> posRangeX = writeIndexerX.positionRange();
+    array<GLfloat, 2> posRangeY = writeIndexerY.positionRange();
+    
+    geometry->addFloatAttribute(0, 3);
+    
+    vector<array<GLfloat, 2> > texRangeU(readIndexersX.size());
+    vector<array<GLfloat, 2> > texRangeV(readIndexersY.size());
+    for (size_t i = 0; i < readIndexersX.size(); i++) {
+        assert(readIndexersX[i]._usage == ISTextureIndexer::Usage::Read);
+        assert(readIndexersY[i]._usage == ISTextureIndexer::Usage::Read);
+        assert(readIndexersX[i].direction() != readIndexersY[i].direction()); //Topological constraint
+        texRangeU[i] = readIndexersX[i].texelRange(readIndexersX[i].texelCount());
+        texRangeV[i] = readIndexersY[i].texelRange(readIndexersY[i].texelCount());
+        geometry->addFloatAttribute(i + 1, 2);
+    }
+    vector<array<GLfloat, 2> > mathRangeX(mathIndexersX.size());
+    vector<array<GLfloat, 2> > mathRangeY(mathIndexersY.size());
+    for (size_t i = 0; i < mathIndexersX.size(); i++) {
+        assert(mathIndexersX[i]._usage == ISTextureIndexer::Usage::Math);
+        assert(mathIndexersY[i]._usage == ISTextureIndexer::Usage::Math);
+        assert(mathIndexersX[i].direction() != mathIndexersY[i].direction()); //Topological constraint
+        mathRangeX[i] = mathIndexersX[i].mathRange();
+        mathRangeY[i] = mathIndexersY[i].mathRange();
+        geometry->addFloatAttribute(i + readIndexersX.size() + 1, 2);
+    }
+    
+    appendV0QuadPos(vec, posRangeX, posRangeY);
+    appendTex<0>(vec, readIndexersX, readIndexersY, texRangeU, texRangeV, xWriteDirection);
+    appendMath<0>(vec, mathIndexersX, mathIndexersY, mathRangeX, mathRangeY, xWriteDirection);
+    appendV1QuadPos(vec, posRangeX, posRangeY);
+    appendTex<1>(vec, readIndexersX, readIndexersY, texRangeU, texRangeV, xWriteDirection);
+    appendMath<1>(vec, mathIndexersX, mathIndexersY, mathRangeX, mathRangeY, xWriteDirection);
+    appendV3QuadPos(vec, posRangeX, posRangeY);
+    appendTex<3>(vec, readIndexersX, readIndexersY, texRangeU, texRangeV, xWriteDirection);
+    appendMath<3>(vec, mathIndexersX, mathIndexersY, mathRangeX, mathRangeY, xWriteDirection);
+
+    appendV1QuadPos(vec, posRangeX, posRangeY);
+    appendTex<1>(vec, readIndexersX, readIndexersY, texRangeU, texRangeV, xWriteDirection);
+    appendMath<1>(vec, mathIndexersX, mathIndexersY, mathRangeX, mathRangeY, xWriteDirection);
+    appendV2QuadPos(vec, posRangeX, posRangeY);
+    appendTex<2>(vec, readIndexersX, readIndexersY, texRangeU, texRangeV, xWriteDirection);
+    appendMath<2>(vec, mathIndexersX, mathIndexersY, mathRangeX, mathRangeY, xWriteDirection);
+    appendV3QuadPos(vec, posRangeX, posRangeY);
+    appendTex<3>(vec, readIndexersX, readIndexersY, texRangeU, texRangeV, xWriteDirection);
+    appendMath<3>(vec, mathIndexersX, mathIndexersY, mathRangeX, mathRangeY, xWriteDirection);
     
     geometry->upload(vec);
 }
--- Image Shader/ISTextureIndexing.h
+++ Image Shader/ISTextureIndexing.h
@@ -1,10 +1,19 @@
-//
-//  ISTextureIndexing.h
-//  Image Shader
-//
-//  Created by William Talmadge on 7/19/14.
-//  Copyright (c) 2014 William Talmadge. All rights reserved.
-//
+/*
+ Copyright (C) 2014  William B. Talmadge
+ 
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
 
 #ifndef __Image_Shader__ISTextureIndexing__
 #define __Image_Shader__ISTextureIndexing__
@@ -14,36 +23,45 @@
 #include <OpenGLES/ES2/gl.h>
 #include <OpenGLES/ES2/glext.h>
 
-enum class ISDirection { Rows, Cols };
+enum class ISDirection { Rows, Cols, Undefined };
 class ISVertexArray;
 struct ISTextureIndexer {
-    ISTextureIndexer() : _stride(1), _offset(0), _rangeStart(0), _rangeEnd(0) { }
+    enum class Usage { Write, Read, Math, Undefined };
+    ISTextureIndexer() : _stride(1), _offset(0), _rangeStart(0), _rangeEnd(0), _direction(ISDirection::Undefined), _usage(Usage::Undefined) { }
     static ISTextureIndexer make() { return ISTextureIndexer(); }
     ISTextureIndexer& from(int rangeStart);
-    ISTextureIndexer& to(int rangeEnd);
-    ISTextureIndexer& by(int stride);
+    ISTextureIndexer& upTo(int rangeEnd);
+    ISTextureIndexer& by(float stride);
     ISTextureIndexer& offset(int offset);
     ISTextureIndexer& alongRows(GLuint texelCount);
+    ISTextureIndexer& alongRows();
     ISTextureIndexer& alongCols(GLuint texelCount);
+    ISTextureIndexer& alongCols();
     ISTextureIndexer& reads();
     ISTextureIndexer& writes();
+    ISTextureIndexer& math();
     bool isPosition();
     bool isColOriented();
     GLuint texelCount();
     ISDirection direction();
     std::array<GLfloat, 2> positionRange();
     std::array<GLfloat, 2> texelRange(size_t scale);
+    std::array<GLfloat, 2> mathRange();
 
+    friend void indexerGeometry(ISVertexArray* geometry, GLuint width, GLuint height, ISTextureIndexer writeIndexer, std::vector<ISTextureIndexer> readIndexers);
+    friend void indexerGeometry2D(ISVertexArray* geometry, ISTextureIndexer writeIndexerX, ISTextureIndexer writeIndexerY, std::vector<ISTextureIndexer> readIndexersX, std::vector<ISTextureIndexer> readIndexersY);
+    friend void indexerGeometry2D(ISVertexArray* geometry, ISTextureIndexer writeIndexerX, ISTextureIndexer writeIndexerY, std::vector<ISTextureIndexer> readIndexersX, std::vector<ISTextureIndexer> readIndexersY, std::vector<ISTextureIndexer> mathIndexersX, std::vector<ISTextureIndexer> mathIndexersY);
 protected:
-    int _stride;
+    float _stride;
     int _offset;
     int _rangeStart;
     int _rangeEnd;
     ISDirection _direction;
-    bool _reads;
+    Usage _usage;
     GLuint _texelCount;
 };
 
 void indexerGeometry(ISVertexArray* geometry, GLuint width, GLuint height, ISTextureIndexer writeIndexer, std::vector<ISTextureIndexer> readIndexers);
-
+void indexerGeometry2D(ISVertexArray* geometry, ISTextureIndexer writeIndexerX, ISTextureIndexer writeIndexerY, std::vector<ISTextureIndexer> readIndexersX, std::vector<ISTextureIndexer> readIndexersY);
+void indexerGeometry2D(ISVertexArray* geometry, ISTextureIndexer writeIndexerX, ISTextureIndexer writeIndexerY, std::vector<ISTextureIndexer> readIndexersX, std::vector<ISTextureIndexer> readIndexersY, std::vector<ISTextureIndexer> mathIndexersX, std::vector<ISTextureIndexer> mathIndexersY);
 #endif /* defined(__Image_Shader__ISTextureIndexing__) */
