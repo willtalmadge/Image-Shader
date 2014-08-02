504fcf1218be7fb1350cd21f8a467e6deb09cd75
willtalmadge <willtalmadge@gmail.com>
2014-08-02 02:07:03 -0600

    Added a 2 tuple macro

--- Image Shader/ISTextureTuple.h
+++ Image Shader/ISTextureTuple.h
@@ -22,16 +22,75 @@
 #include <iostream>
 #include <vector>
 #include "ISTexture.h"
+#include "ISRect.h"
 
 class ISPipeline;
 struct ISTextureTuple {
     void join(ISTextureTuple* previous);
     void deleteTextures();
     virtual GLuint textureUnitsUsed() const = 0;
+    virtual ISSize size() const = 0;
     virtual ~ISTextureTuple();
     ISPipeline pipeline();
     void map(std::function<void (ISTextureRef)> f); //TODO: consider making this only accessible to ISPipeline
-protected:
+//FIXME: restore protected:
     std::vector<ISTextureRef> _elements;
 };
+
+#define IS_2TUPLE_HEADER(TUPLE_NAME, TEXTURE1_NAME, TEXTURE2_NAME) \
+struct TUPLE_NAME##Bindable { \
+    virtual GLuint TEXTURE1_NAME##BindingTarget() const = 0; \
+    virtual GLuint TEXTURE2_NAME##BindingTarget() const = 0; \
+};\
+struct TUPLE_NAME : ISTextureTuple { \
+    template<class T> \
+    void setup(GLuint width, GLuint height); \
+    void setup(std::unique_ptr<ISSingleton>& TEXTURE1_NAME, std::unique_ptr<ISSingleton>& TEXTURE2_NAME); \
+void setup(ISTextureRef TEXTURE1_NAME, ISTextureRef TEXTURE2_NAME);\
+    ISTextureRef get_##TEXTURE1_NAME() const; \
+    ISTextureRef get_##TEXTURE2_NAME() const; \
+    void set_##TEXTURE1_NAME(ISTextureRef TEXTURE1_NAME) { _elements[0] = TEXTURE1_NAME; } \
+    void set_##TEXTURE2_NAME(ISTextureRef TEXTURE2_NAME) { _elements[1] = TEXTURE2_NAME; } \
+    void bind(const TUPLE_NAME##Bindable* drawable); \
+    GLuint textureUnitsUsed() const { return 2; }; \
+    ISSize size() const; \
+}; \
+template<class T> \
+void TUPLE_NAME::setup(GLuint width, GLuint height) { \
+    _elements = {NULL, NULL}; \
+    ISTextureRef TEXTURE1_NAME = T::make(width, height); \
+    ISTextureRef TEXTURE2_NAME = T::make(width, height); \
+    set_##TEXTURE1_NAME(TEXTURE1_NAME); \
+    set_##TEXTURE2_NAME(TEXTURE2_NAME); \
+}
+
+#define IS_2TUPLE_IMPLEMENTATION(TUPLE_NAME, TEXTURE1_NAME, TEXTURE2_NAME) \
+void TUPLE_NAME::setup(std::unique_ptr<ISSingleton>& TEXTURE1_NAME, std::unique_ptr<ISSingleton>& TEXTURE2_NAME) { \
+_elements = {NULL, NULL}; \
+set_##TEXTURE1_NAME(TEXTURE1_NAME->getTexture()); \
+set_##TEXTURE2_NAME(TEXTURE2_NAME->getTexture()); \
+} \
+void TUPLE_NAME::setup(ISTextureRef TEXTURE1_NAME, ISTextureRef TEXTURE2_NAME) { \
+_elements = {NULL, NULL}; \
+set_##TEXTURE1_NAME(TEXTURE1_NAME); \
+set_##TEXTURE2_NAME(TEXTURE2_NAME); \
+} \
+ISTextureRef TUPLE_NAME::get_##TEXTURE1_NAME() const { \
+    return _elements[0]; \
+} \
+ISTextureRef TUPLE_NAME::get_##TEXTURE2_NAME() const { \
+    return _elements[1]; \
+} \
+void TUPLE_NAME::bind(const TUPLE_NAME##Bindable* drawable) { \
+    ISTextureRef TEXTURE1_NAME = get_##TEXTURE1_NAME(); \
+    ISTextureRef TEXTURE2_NAME = get_##TEXTURE2_NAME(); \
+    TEXTURE1_NAME->bindToShader(drawable->TEXTURE1_NAME##BindingTarget(), 0); \
+    TEXTURE2_NAME->bindToShader(drawable->TEXTURE2_NAME##BindingTarget(), 1); \
+} \
+ISSize TUPLE_NAME::size() const { \
+    auto size = ISSize().width(get_##TEXTURE1_NAME()->width()) \
+    .height(get_##TEXTURE1_NAME()->height()); \
+    return size; \
+}
+
 #endif /* defined(__Image_Shader__ISTextureTuple__) */
